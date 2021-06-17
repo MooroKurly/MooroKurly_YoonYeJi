b95429d3ad554c3188966363c72d34ab7f420f49
yejiyun <20172062@sungshin.ac.kr>
2021-06-18 05:17:44 +0900

    [add] String strikeThrough extension

---
+++ moorukurly_codebase/Source/Extension/String + strikeThrough.swift
@@ -0,0 +1,16 @@
+//
+//  String + strikeThrough.swift
+//  moorukurly_codebase
+//
+//  Created by 윤예지 on 2021/06/18.
+//
+
+import UIKit
+
+extension String {
+    func strikeThrough() -> NSAttributedString {
+            let attributeString: NSMutableAttributedString = NSMutableAttributedString(string: self)
+            attributeString.addAttribute(NSAttributedString.Key.strikethroughStyle, value: NSUnderlineStyle.single.rawValue, range: NSMakeRange(0, attributeString.length))
+            return attributeString
+    }
+}
